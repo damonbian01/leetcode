ccd8b2663b3137ed0881f87c443ff7f11e5568b9
biantao <biantao@sogou-inc.com>
2016-02-22 17:15:56 +0800

    add median algorithm

---
+++ Median/src/main/java/org/Median/App.java
@@ -0,0 +1,22 @@
+package org.Median;
+
+/**
+ * 计算两个有序数组的Median
+ *
+ */
+public class App 
+{
+	private double findMedianSortedArrays(int [] nums1, int [] nums2) {
+		return 0;
+	}
+	
+    public static void main( String[] args )
+    {
+    	/*input params*/
+    	int [] nums1 = {1,2,3,4,5,6};
+    	int [] nums2 = {2,3,4,9,13,12,45,56,67};
+    	
+    	App start = new App();
+    	System.out.println("the median of two sorted array is "+start.findMedianSortedArrays(nums1, nums2));
+    }
+}
