bb551de2b10ad09f6c83ca932ea63214bb593d12
damonbian01 <870680723@qq.com>
2016-02-25 09:34:16 +0800

    add module zipzag

---
+++ ZipZag/src/main/java/org/ZipZag/App.java
@@ -0,0 +1,19 @@
+package org.ZipZag;
+
+/**
+ * 循环对角线
+ * 
+ * 0	 	 	 	8	 	 	 	16	 	 	 
+ * 1	 	 	7	9	 	 	15	17	 	 	 
+ * 2	 	6	 	10	 	14	 	18	 	 	 
+ * 3	5	 	 	11	13	 	 	19	 	 	 
+ * 4	 	 	 	12	 	 	 	20
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
